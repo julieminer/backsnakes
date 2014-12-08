8ec152505a7f8075f470f200ee500bf66cb5aa26
Jacob Miner <zyko101@gmail.com>
2014-12-07 23:11:20 -0800

    Revert "Directories Galore"
    This reverts commit 3a2b427abb01f64f0b8bacbce5c3d2671648e60d.

--- Server/run.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-import os
-
-results = Popen("pwd", shell=True, stdout=PIPE).stdout.read()
-results = "nohup python " + results[:-1] + "/backjake.py &"
-os.system(results)
